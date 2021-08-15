54143e0dbc85876ede7f64932692cde79050ada0
vijay <vijayanandmit@hotmail.com>
2021-08-15 08:13:55 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,233 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+from ibapi.order import *
+from threading import Timer
+
+port = 7497  # Simulated + TWS
+#port = 4002 # Simulated + Gateway
+
+#buy ES
+class longES(EWrapper, EClient):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.done = False
+        self.nextOrderId = ""
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    def nextValidId(self, orderId):
+        self.nextOrderId = orderId
+        self.start()
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId,
+                    whyHeld, mktCapPrice):
+        print("OrderStatus. Id: ", orderId, ", Status: ", status, ", Filled:", filled, ", Remaining: ", remaining,
+              ", LastFillPrice: ", lastFillPrice)
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print("OpenOrder. ID:", orderId, contract.symbol, contract.secType, "@", contract.exchange, ":", order.action,
+              order.orderType, order.totalQuantity, orderState.status)
+
+    def execDetails(self, reqId, contract, execution):
+        print("ExecDetails. ", reqId, contract.symbol, contract.secType, contract.currency, execution.execId,
+              execution.orderId, execution.shares, execution.lastLiquidity)
+
+
+    def start(self):
+
+        #define the contract
+        contract = Contract()
+        contract.symbol = "ES"
+        contract.secType = "FUT"
+        contract.exchange = "GLOBEX"
+        contract.currency = "USD"
+        contract.lastTradeDateOrContractMonth = "202109"
+
+        #place the order
+        order = Order()
+        order.action = "BUY"
+        order.totalQuantity = 1
+        order.orderType = "MKT"
+        self.placeOrder(self.nextOrderId, contract, order)
+
+    def stop(self):
+        self.done = True
+        self.disconnect()
+
+#buy NQ
+class buyNQ(EWrapper, EClient):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.done = False
+        self.nextOrderId = ""
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    def nextValidId(self, orderId):
+        self.nextOrderId = orderId
+        self.start()
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId,
+                    whyHeld, mktCapPrice):
+        print("OrderStatus. Id: ", orderId, ", Status: ", status, ", Filled:", filled, ", Remaining: ", remaining,
+              ", LastFillPrice: ", lastFillPrice)
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print("OpenOrder. ID:", orderId, contract.symbol, contract.secType, "@", contract.exchange, ":", order.action,
+              order.orderType, order.totalQuantity, orderState.status)
+
+    def execDetails(self, reqId, contract, execution):
+        print("ExecDetails. ", reqId, contract.symbol, contract.secType, contract.currency, execution.execId,
+              execution.orderId, execution.shares, execution.lastLiquidity)
+
+
+    def start(self):
+
+        #define the contract
+        contract = Contract()
+        contract.symbol = "MNQ"
+        contract.secType = "FUT"
+        contract.exchange = "GLOBEX"
+        contract.currency = "USD"
+        contract.lastTradeDateOrContractMonth = "202109"
+
+        #place the order
+        order = Order()
+        order.action = "SELL"   #"BUY"
+        order.totalQuantity = 1
+        order.orderType = "MKT"
+        self.placeOrder(self.nextOrderId, contract, order)
+
+    def stop(self):
+        self.done = True
+        self.disconnect()
+
+class EScall(EWrapper, EClient):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.done = False
+        self.nextOrderId = ""
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    def nextValidId(self, orderId):
+        self.nextOrderId = orderId
+        self.start()
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId,
+                    whyHeld, mktCapPrice):
+        print("OrderStatus. Id: ", orderId, ", Status: ", status, ", Filled:", filled, ", Remaining: ", remaining,
+              ", LastFillPrice: ", lastFillPrice)
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print("OpenOrder. ID:", orderId, contract.symbol, contract.secType, "@", contract.exchange, ":", order.action,
+              order.orderType, order.totalQuantity, orderState.status)
+
+    def execDetails(self, reqId, contract, execution):
+        print("ExecDetails. ", reqId, contract.symbol, contract.secType, contract.currency, execution.execId,
+              execution.orderId, execution.shares, execution.lastLiquidity)
+
+
+    def start(self):
+        #define the contract
+        contract = Contract()
+        contract.symbol = ticker
+        contract.secType = "FOP"
+        contract.exchange = "GLOBEX"
+        contract.currency = "USD"
+        contract.multiplier = "50"
+
+        #place the order
+        order = Order()
+        order.action = action
+        contract.strike = strike
+        contract.right = putcall
+        contract.lastTradeDateOrContractMonth = expr
+        order.totalQuantity = 1
+        order.orderType = "MKT"
+        self.placeOrder(self.nextOrderId, contract, order)
+
+    def stop(self):
+        self.done = True
+        self.disconnect()
+
+class SellEScall(EWrapper, EClient):
+
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.done = False
+        self.nextOrderId = ""
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    def nextValidId(self, orderId):
+        self.nextOrderId = orderId
+        self.start()
+
+    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId,
+                    whyHeld, mktCapPrice):
+        print("OrderStatus. Id: ", orderId, ", Status: ", status, ", Filled:", filled, ", Remaining: ", remaining,
+              ", LastFillPrice: ", lastFillPrice)
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print("OpenOrder. ID:", orderId, contract.symbol, contract.secType, "@", contract.exchange, ":", order.action,
+              order.orderType, order.totalQuantity, orderState.status)
+
+    def execDetails(self, reqId, contract, execution):
+        print("ExecDetails. ", reqId, contract.symbol, contract.secType, contract.currency, execution.execId,
+              execution.orderId, execution.shares, execution.lastLiquidity)
+
+
+    def start(self):
+
+        #define the contract
+        contract = Contract()
+        contract.symbol = "ES"
+        contract.secType = "FOP"
+        contract.exchange = "GLOBEX"
+        contract.currency = "USD"
+        contract.multiplier = "50"
+
+        #place the order
+        order = Order()
+        order.action = "sell"
+        contract.right = "C"
+        contract.strike = 4380
+        contract.lastTradeDateOrContractMonth = "20210813"
+        order.totalQuantity = 1
+        order.orderType = "MKT"
+        self.placeOrder(self.nextOrderId, contract, order)
+
+    def stop(self):
+        self.done = True
+        self.disconnect()
+
+ticker = "ES"
+action = "sell"
+putcall = "P"
+strike = 4340
+expr = "20210806"
+
+#execute the classes
+def main():
+#    app = longES()
+#    app = SellEScall()
+#    app = buyCalls()
+#    app = buyNQ()
+    app = EScall()
+
+    app.nextOrderId = 0
+    app.connect("127.0.0.1", port, 9)  # IB Gateway PaperTrading
+    Timer(3, app.stop).start()
+    app.run()
+
+if __name__ == "__main__":
+    main()
